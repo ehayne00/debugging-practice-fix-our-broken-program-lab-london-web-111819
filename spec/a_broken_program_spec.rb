14fbfa8dbacf213584ca9ed0decfa7d50eef2e70
Emily Haynes <ehayne00@hotmail.com>
2019-11-15 20:07:47 +0000

    Automatically backed up by Learn

--- spec/a_broken_program_spec.rb
+++ spec/a_broken_program_spec.rb
@@ -3,7 +3,7 @@ describe 'A broken program' do
   it 'not to raise an error when loaded' do
     expect{
       load 'lib/a_working_program.rb'
-    }.to not raise_error
+    }.to_not raise_error
   end
 
 end
